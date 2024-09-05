9557a7f9a5cf6d57ce68a35e5a0f7cc76a405032
John Ryan <ryjohn@google.com>
2024-09-05 13:31:46 -0700

    Use an ImplicitlyAnimatedWidget

--- animations/step_02/lib/scoreboard.dart
+++ animations/step_02/lib/scoreboard.dart
@@ -18,13 +18,32 @@ class Scoreboard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           for (var i = 0; i < totalQuestions; i++)
-            Icon(
-              Icons.star,
-              size: 50,
-              color:
-                  score < i + 1 ? Colors.grey.shade400 : Colors.yellow.shade700,
+            AnimatedStar(
+              isActive: score > i,
             )
         ],
+      ),
+    );
+  }
+}
+
+class AnimatedStar extends StatelessWidget {
+  final bool isActive;
+  final Duration _duration = const Duration(milliseconds: 1000);
+  final Color _deactivatedColor = Colors.grey.shade400;
+  final Color _activatedColor = Colors.yellow.shade700;
+
+  AnimatedStar({super.key, required this.isActive});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedScale(
+      scale: isActive ? 1.0 : 0.5,
+      duration: _duration,
+      child: Icon(
+        Icons.star,
+        size: 50,
+        color: isActive ? _activatedColor : _deactivatedColor,
       ),
     );
   }
